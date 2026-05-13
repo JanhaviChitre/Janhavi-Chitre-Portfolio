d43b0dd2b3e4b92e82d72751b419c5144f02ef5b
JanhaviChitre <janhavi.2406417201@vcet.edu.in>
2026-05-13 13:54:00 +0530

    Improved UI

--- src/app/page.js
+++ src/app/page.js
@@ -178,7 +178,7 @@ export default function Home() {
           </a>
 
           <a
-            href="/JanhaviResume.pdf"
+            href="/JanhaviChitreResume.pdf"
             download
             className="
               px-5 py-2
